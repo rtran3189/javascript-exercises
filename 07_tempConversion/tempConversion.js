b1003c995f00e38a52a0b493bec3f8d7f570fec7
Richard Tran <5017216+rtran3189@users.noreply.github.com>
2023-03-14 15:04:02 -0600

    all exercises finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const convertToCelsius = function() {
+/**
+ * Converts Fahrenheit to Celsius.
+ * @param {number} temp The temperature in Fahrenheit.
+ * @returns Temperature in Celsius.
+ */
+const convertToCelsius = function (temp) {
+  tempToCelsius = (temp - 32) * (5 / 9);
+  result = Math.round(tempToCelsius * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+/**
+ * Converts Celsius to Fahrenheit.
+ * @param {number} temp The temperature in Celsius.
+ * @returns Temperature in Fahrenheit.
+ */
+const convertToFahrenheit = function (temp) {
+  tempToFahrenheit = temp * (9 / 5) + 32;
+  result = Math.round(tempToFahrenheit * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
